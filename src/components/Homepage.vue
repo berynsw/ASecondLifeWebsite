1823c090f3306063d3efedcb438b5f2ee2a887b9
Keili <keilirivera99@gmail.com>
2021-02-21 11:26:24 -0800

    added new user section to homepage and newuserguide file

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
         <UpcycleSearch />
+        <body>
+           <div class="name">New to Upcycling?</div>
+           <div class="img-boots"></div>
+           <p class="text">Explore our mission and community. A Second Life is dedicated to reducing waste through simple upcycle projects. What's an upcycle? Don't know where to start? Learn more by clicking below.</p>
+           <button class="NewUser" v-on:click="NewUser">New User Guide</button>
+
+        </body>
         <IdeaForm />
     </div>
 </template>
@@ -20,5 +27,56 @@ export default {
 </script>
 
 <style scoped>
+body{
+    width: 100vw;
+    height: 590px;
+    padding: 50px 80px;
+    background-color: #dad6ce;
+    font-family: 'Courier New', Courier, monospace;
+    position: relative;
+}
+body .name{
+    font-size: 50px;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    width: 800px;
+    position: absolute;
+    top: 50px;
+    left: 100px;
+}
+body .text{
+    font-size: 30px;
+    width: 500px;
+    position: absolute;
+    top: 160px;
+    left: 100px;
+}
+body .img-boots{
+    background-image: url("../assets/boots.jpg");
+    position: absolute;
+    top: 100px;
+    right:100px;
+}
+body .NewUser{
+    width: 300px;
+    height: 40px;
+    margin: 25px 0px;
+    border-radius: 25px;
+    border: 5px solid transparent;
+    border-color: #777;
+    font-size: 20pt;
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+    opacity: 80%; 
+    position: absolute;
+    bottom:7px;
+    left: 100px;
+    transition: 0.4s;
+}
+body .NewUser.active, 
+body .NewUser:hover{
+    background-color: #555;
+    border-color: #c58db7;
+    opacity: 90%;
+}
 
 </style>
---
+++ src/components/NewUserGuide.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'NewUserGuide'
+}
+</script>
+
+<style scoped>
+
+</style>
