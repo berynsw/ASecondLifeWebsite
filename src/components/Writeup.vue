fc34b2a6ce6b67926fb116936720eda0666a9182
staubwb <staubwb@wwu.edu>
2021-03-15 20:47:38 -0700

    adding writeup page

---
+++ src/components/Writeup.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>
+            Writeup
+        </p>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Writeup',
+}
+</script>
+
+<style scoped>
+    p{
+        color: black;
+        font-size: 6vw;
+    }
+</style>
