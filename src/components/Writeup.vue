78b1e1d79500d721c8b7567a31e7232a36cb0154
staubwb <staubwb@wwu.edu>
2021-03-17 20:33:54 -0700

    i love busy work

--- src/components/Writeup.vue
+++ src/components/Writeup.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="main">
+        
+        
         <div class="column">
             <p class="title">
                 <span class="question" id="header">A Second Life</span>
                 <br>
                 <br>
-                Katie Werner wernerk5@wwu.edu, Keili, Ashima, Beryn Staub-Waldenberg staubwb@wwu.edu
-                <br>
                 CSCI 436, Winter 2021, Dr. Shameem Ahmed
+                
+                <br>
+                Katie Werner wernerk5@wwu.edu, Keili riverak3@wwu.edu, Ashima shrivaa@wwu.edu, Beryn Staub-Waldenberg staubwb@wwu.edu
             </p>
             <br>
             
+            <br>
+            <div class="section">Project Description:</div>
             <br>
             <p>
                 <span class="question">
@@ -51,6 +56,10 @@
                 <span class="question">
                     How many people (should be at least millions of people) will be benefited from your solution? Give evidence(s) of your claim by giving statistics from the previous studies or from any reputed organizations such as WHO, UN, CDC:
                 </span>
+                According to edtimes the fashion industry is responsible for 10% of global carbon emissions which is more than the emissions of international flights. 1.2 billion tons of CO2 is released in the atmosphere every year by the fast fashion industry. The dyes and other chemicals used to cause massive environmental damage. Just by reusing old clothes or thrifting them for 4-5 months, carbon emission by 4%. This was just for clothes, now think what would plastic do to our environment ? Moreover, The UN states that “Should the global population reach 9.6 billion by 2050, the equivalent of almost three planets could be required to provide the natural resources needed to sustain current lifestyles”. Serious problem right ? 
+
+With our project we are targeting to affect and benefit not just millions but billions of people and more then that, we are targeting to benefit the environment! In today’s world, major changes are dependent on youth voice and change towards sustainability, global warming and carbon emission starts with us! 
+
             </p>
             <br>
             <p>
@@ -74,39 +83,162 @@
                 We decided to use a website as the platform for our solution. We chose to make a website instead of an app so that our solution could reach more users including the ones that might not have access to a smartphone.
             </p>
             <br>
+            
+            <br>
+            <div class="section">Research Methodology:</div>
+            <br>
+            
             <p>
                 <span class="question">
                     Explain your systematic literature review. How many papers did you read? Write a summary of your findings:
                 </span>
-
+                As a group we collectively read 10 research papers on related topics to our project. Through our research we found two main aspects of the papers that related most to our project. These two findings were the urgency of the waste crisis and how teaching the practice of upcycling is a great way to teach the importance of sustainability to all ages. Through our research, we learned that if waste production does not peak this century we will have negative irreversible effects on the environment. Knowing this has fueled our passion for creating a website that helps to inform people on simple ways to reduce their production of waste and teach them how to be more sustainable in their daily lives. The other big takeaway from our research was how important it is to teach the idea of upcycling. In one article, it explained how children from two different communities were represented with items that were normally seen as trash and asked them to create something out of them. They were able to come up with ideas such as creating a couch and a snowglobe out of items they normally see as trash. This helped to support our thoughts of if you are able to reframe someones thinking about a single use item they will come up with another way to use it just like an upcycle and in turn cut down on the amount of garbage they put out into the environment.
+                <br>
+                <br>
+                Group Prototype 1:
+                <br>
+                <img :src="require(`@/assets/image1.png`)" class="img">
+                <img :src="require(`@/assets/image2.png`)" class="img">
+                <img :src="require(`@/assets/image3.png`)" class="img">
+                <img :src="require(`@/assets/image4.png`)" class="img">
             </p>
             <br>
             <p>
                 <span class="question">
                     Explain your system analysis. How many systems did you analyze? Write a summary of your findings:
                 </span>
-                
+                As a group we analyzed 12 different systems, some being apps and some being websites. From this analysis, we found some similar websites/apps like ours, A Second Life, but none that were exactly the same concept. From the research, we decided something that would be important to add to our page, that we saw on other systems, was a resource page. Our research page would include information on where to take items no longer being used so they can be taken care of in a sustainable way and information on brands that use and support sustainable practices.
+                <br>
+                <br>
+                Group Prototype 2:
+                <br>
+                <img :src="require(`@/assets/image5.png`)" class="img">
+                <img :src="require(`@/assets/image6.png`)" class="img">
+                <img :src="require(`@/assets/image7.png`)" class="img">
             </p>
             <br>
             <p>
                 <span class="question">
                     Explain your user study. How many interviews did you conduct? Write a summary of your findings:
                 </span>
-                
+                As a group we conducted four user studies. One of the biggest findings of our interviews were that many people were not as familiar with the idea of upcycles as we thought. Learning this was very helpful, now we knew that we would need to have a comprehensive explanation of what an upcycle is and how making them can have a positive impact on the environment. With this information we decided to implement a new user guide page. This page would end up including information on what an upcycle is, how it helps the planet, and how to get started on our website. We also decided to play around with the search/explore bar on our front navigation, but ultimately we took out the search bar in our final prototype.
+                <br>
+                <br>
+                Group Prototype 3:
+                <br>
+                <img :src="require(`@/assets/image8.png`)" class="img">
+                <br>
+                <img :src="require(`@/assets/image9.png`)" class="img">
+                <br>
+                <img :src="require(`@/assets/image10.png`)" class="img">
+                <br>
+                <img :src="require(`@/assets/image11.png`)" class="img">
+                <br>
+                <img :src="require(`@/assets/image12.png`)" class="img">
             </p>
+            <br>
+            
+            <br>
+            <div class="section">Final Product:</div>
+            <br>
+            
+            <p>
+                <span class="question">
+                    Home Page: 
+                </span>
+                
+                <ul class="list">
+                    
+                    <li>Navigation Bar: that is on each page and takes users to the various pages on the site. Links to pages for: Explore page, Resources page, About page, Contact page, and a page for our project writeup.</li>
+                    <li>Banner/Upcycle Search: The upcycle search box is the first thing that users see when arriving at our website, and by clicking the “start creating button”, users will be redirected to the Explore page</li>
+                    <li>Upcycles of the week: This shows a few featured upcycle ideas on a carousel that users can rotate through. Clicking on a specific upcycle from the carousel will redirect the user to the instructions on how to create the upcycle.</li>
+                    <li>New User Guide Link: Short description of our mission, and a link to a guide for new users on our New User guide page</li>
+                    <li>Upcycle Idea Form: This section shows up on each page. Users can contribute to our website by entering their email address and an upcycle idea of their own, and upon submitting the idea, the website developers can choose to add that idea to our current website.</li>
+                    <li>Footer: This section shows up at the bottom of each page, and provides the user the same links as in the header, so users can easily navigate the website without having to scroll all the way back to the top of the page.</li>
+                    
+                </ul>
+            </p>
+
+
+
             <br>
             <p>
                 <span class="question">
-                    Discuss in detail the final prototype (every feature):
+                    Explore: 
                 </span>
-                
+
+                <ul class="list">
+                    <li>This page lets users scroll through various categories of items to upcycle, in order to browse upcycling ideas. Each section has a picture of the category, as well as a dropdown select box that lists all of the upcycle ideas of that specific category. Once a user clicks on an idea they would like to see instructions for, a “create” button pops up and users can click on it to navigate to the specific upcycle page.</li>
+                    <li>Categories: Plastic, tin, glass, food waste, cardboard, and aluminum.</li>
+
+                </ul>
             </p>
+
+
+            <br>
+            <p>
+                <span class="question">
+                    About: 
+                </span>
+                The about page explains our motive for this website and shows our passion for this project. Here we discuss our mission, the start of this project and it’s future.
+            </p>
+
+
+            <br>
+            <p>
+                <span class="question">
+                    Resources: 
+                </span>
+                <ul class="list">
+                    <li>With our resources page we aim to educate our users about a better sustainable lifestyle. Sometimes, people do not have time to make the best out of a used item and are unable to perform upcycling. In this case we provide our user with all the resources they require to still maintain a sustainable lifestyle.</li>
+                    <li>Donation: We provide nearby donation centers that accept all kinds of donations. It is better to donate your used material and give them a new home then to just throw them away. This is a good cause and will also promote a better lifestyle habit.</li>
+                    <li>Companies involved in sustainable recycling: Some companies are involved in the sustainable practice of returning used containers purchased by customers back to their company. Other companies are involved in sustainable recycling practices making their products from recycled materials. Here we list some companies that are involved in these sustainable practices. </li>
+
+                </ul>
+            </p>
+
+
+
+            <br>
+            <p>
+                <span class="question">
+                    Contact: 
+                </span>
+                This page provides users with contact information, and says the area in which we are located.
+            </p>
+
+
+            
+            <br>
+            <p>
+                <span class="question">
+                    New User Guide: 
+                </span>
+                <ul class="list">
+                    <li>New user guide page is for all the new users who visit our website. </li>
+                    <li>We welcome our users and give a very brief introduction for our website.</li>
+                    <li>Upcycle: We make our users familiar with the concept of upcycling and educate them towards what an upcycle is.</li>
+                    <li>Getting started: Finally we give our users three simple steps to get started with their first upcycle experience.</li>
+                </ul>
+            </p>
+
+
+
+            
+            <br>
+            <p>
+                <span class="question">
+                    Upcycle Instruction Page: 
+                </span>
+                This page gives details about the upcycle chosen, including the title of the upcycle, the item, category of the item, the environmental impact the item would normally have when thrown into the garbage, necessary supplies to complete the project, and instructions on how to create the upcycle. It also shows a picture of the finished upcycle.
+            </p>
+
             <br>
             <p>
                 <span class="question">
                     Discuss all the technologies/software/API you used:
                 </span>
-                Our app is mainly constructed using Node.js. More specifically, we used Vue.js on the front end and a simple express server on the back end. We have a JSON file that stores the data for specific upcycles. That data is delivered to the front end by an http request to the express server. Lastly, our code is being hosted on Heroku Cloud. 
+                For the front-end, we used Vue.js which is a javascript library. We used the component concept to reuse our environment and to link our pages together and NPM core modules to run our server. We used Vue Router to add routes to our website to make the flow of our working environment easier. To transfer data from parent component to child component, we used the Vue props which acts as a global variable for all the different pages and sends data from one page to another. And finally for UI/UX we used HTML and CSS to make it look more presentable. For back-end, to fetch our upcycle data we used Node.js. We created a mini database using node and called those data in our upcycle page to show different types of upcycles. That data is delivered to the front end by an http request to the express server that we created. 
             </p>
             <br>
             <p>
@@ -116,6 +248,11 @@
                 <a href="https://github.com/berynsw/ASecondLifeWebsite" target="_blank">A Second Life's Github</a>
             </p>
             <br>
+            
+            <br>
+            <div class="section">Future Work:</div>
+            <br>
+            
             <p>
                 <span class="question">
                     How could your work be extended and why would that be useful?
@@ -124,6 +261,13 @@
             </p>
 
 
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="section">Thanks you for checking out our site!</div>
+            <br>
+
             
         </div>
     </div>
@@ -131,11 +275,25 @@
 
 <script>
     export default {
-    name: 'Writeup',
+        name: 'Writeup',
     }
 </script>
 
 <style scoped>
+    .list{
+        margin-left: 5vw;
+        margin-top: 2vh;
+    }
+    .section{
+       font-size: 1.45vw; 
+       font-weight: bold;
+        text-align: center;
+    }
+    .img{
+        max-width: 40vw;
+        max-height: 40vh;
+    }
+    
     .main{
         background-color: #a3ceae;
         color:black;
@@ -152,10 +310,10 @@
     }
     .title{
         text-align: center;
-        font-size: 1.1vw;
+        font-size: 1.2vw;
     }
     #header{
-        font-size: 1.35vw;
+        font-size: 1.45vw;
     }
     .question{
         font-weight: bold;
